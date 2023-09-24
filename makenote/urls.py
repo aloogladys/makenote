5c6f56cdd39d5aeb0ccccb9122344aaa88a9b80f
aloogladys <aloogladys@gmail.com>
2023-09-24 10:36:58 +0300

    login page

--- makenote/urls.py
+++ makenote/urls.py
@@ -3,15 +3,20 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from makenote_app.views import main,home
+from makenote_app.views import home,register,login,create,error,success
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
-    path('home', home),
+    path('', home),
+    path('login/', login),
+    path('register/', register),
+    path('create/', create),
+    path('success/', success),
+    path('error/', error),
+    
     
 
 
--- makenote_app/views.py
+++ makenote_app/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
+from .models import User   
 
 def login(request):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+            return render(request,'create.html')
+        except:
+            return render(request,'error.html')
     return render(request,'login.html')
 
-def main(request):
-    return render(request,'main.html')
+def register(request):
+
+    if request.method == 'POST':
+        User.objects.create(
+            username=request.POST['username'],
+            password=request.POST['password'])
+        return render(request,'success.html')
+
+    return render(request,'register.html')
 
 def home(request):
     return render(request,'home.html')
+
+def create(request):
+    return render(request,'create.html')
+
+def success(request):
+    return render(request,'success.html')
+
+def error(request):
+    return render(request,'error.html')
 
 
 
